8e5c2e0929dca67bd7406d67198f5dd212e0538b
rspber <kopropa@onet.pl>
2024-02-18 21:55:28 +0100

    revert setup changes

--- User_Setups/Setup42_ILI9341_ESP32.h
+++ User_Setups/Setup42_ILI9341_ESP32.h
@@ -7,11 +7,8 @@
 #define TFT_MOSI 23
 #define TFT_SCLK 18
 #define TFT_CS   15  // Chip select control pin
-#define TFT_DC   26  // Data Command control pin
-#define TFT_RST  27  // Reset pin (could connect to RST pin)
-
-//#define TFT_DC    2  // Data Command control pin
-//#define TFT_RST   4  // Reset pin (could connect to RST pin)
+#define TFT_DC    2  // Data Command control pin
+#define TFT_RST   4  // Reset pin (could connect to RST pin)
 
 // Optional touch screen chip select
 //#define TOUCH_CS 5 // Chip select pin (T_CS) of touch screen
@@ -29,7 +26,7 @@
 // TFT SPI clock frequency
 // #define SPI_FREQUENCY  20000000
 // #define SPI_FREQUENCY  27000000
-#define SPI_FREQUENCY  80000000
+#define SPI_FREQUENCY  40000000
 // #define SPI_FREQUENCY  80000000
 
 // Optional reduced SPI frequency for reading TFT
