dcc55e7601e80293fc695b365a9b598271c08150
rspber <kopropa@onet.pl>
2024-11-01 17:58:41 +0100

    RM68140 driver compiled

--- TFT_Drivers/RM68140_Rotation.hh
+++ TFT_Drivers/RM68140_Rotation.hh
@@ -6,20 +6,20 @@
   rotation = m % 4;
   switch (rotation) {
     case 0: // Portrait
-      setMDCTL((0x00 ^ REV) | TFT_MAD_BGR);
-      T[1] |= ((MAD_SS ^ REV) << 4);
+      setMADCTL((0x00 ^ REV) | TFT_MAD_BGR);
+      T[1] |= ((TFT_MAD_SS ^ REV) << 4);
     break;
     case 1: // Landscape (Portrait + 90)
-      setMADCTL((MAD_YX ^ REV) | TFT_MAD_BGR);
+      setMADCTL((TFT_MAD_YX ^ REV) | TFT_MAD_BGR);
       T[1] |= 0x00;
     break;
     case 2: // Inverter portrait
       setMADCTL((0x00 ^ REV) | TFT_MAD_BGR);
-      T[1] |= ((MAD_GS ^ REV) << 6);
+      T[1] |= ((TFT_MAD_GS ^ REV) << 6);
     break;
     case 3: // Inverted landscape
-      setMADCTL((MAD_YX ^ REV) | TFT_MAD_BGR);
-      T[1] |= ((MAD_SS ^ REV) << 4) | ((MAD_GS ^ REV) << 6);
+      setMADCTL((TFT_MAD_YX ^ REV) | TFT_MAD_BGR);
+      T[1] |= ((TFT_MAD_SS ^ REV) << 4) | ((TFT_MAD_GS ^ REV) << 6);
     break;
   }
   sendCmdData(0xB6, T, 3);
--- User_Setup_Select.h
+++ User_Setup_Select.h
@@ -48,7 +48,7 @@
 //#include <User_Setups/Setup17_ePaper.h>            // Setup file for ESP8266 and any Waveshare ePaper display
 //#include <User_Setups/Setup18_ST7789.h>            // Setup file for ESP8266 configured for ST7789
 
-//#include <User_Setups/Setup19_RM68140_Parallel.h>	 // Setup file configured for RM68140 with parallel bus
+#include <User_Setups/Setup19_RM68140_Parallel.h>	 // Setup file configured for RM68140 with parallel bus
 
 //#include <User_Setups/Setup20_ILI9488.h>           // Setup file for ESP8266 and ILI9488 SPI bus TFT
 //#include <User_Setups/Setup21_ILI9488.h>           // Setup file for ESP32 and ILI9488 SPI bus TFT
@@ -107,7 +107,7 @@
 //#include <User_Setups/Setup100_RP2040_ILI9488_parallel.h> // Setup file for Pico/RP2040 with 8-bit parallel ILI9488
 //#include <User_Setups/Setup101_RP2040_ILI9481_parallel.h> // Setup file for Pico/RP2040 with 8-bit parallel ILI9481
 //#include <User_Setups/Setup102_RP2040_ILI9341_parallel.h> // Setup file for Pico/RP2040 with 8-bit parallel ILI9341
-#include <User_Setups/Setup103_RP2040_ILI9486_parallel.h> // Setup file for Pico/RP2040 with 8-bit parallel ILI9486
+//#include <User_Setups/Setup103_RP2040_ILI9486_parallel.h> // Setup file for Pico/RP2040 with 8-bit parallel ILI9486
 //#include <User_Setups/Setup104_RP2040_ST7796_parallel.h>  // Setup file for Pico/RP2040 with 8-bit parallel ST7796
 
 //#include <User_Setups/Setup105_RP2040_ST7796_16bit_parallel.h>  // Setup file for RP2040 16-bit parallel display
